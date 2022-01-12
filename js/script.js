0a2343156bec25d4cc92c2c80b5b1763874c3875
Ale Ingercher <aleingercher@gmail.com>
2022-01-12 09:43:34 -0300

    Menu aparece al scrollear

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-const menu = document.querySelector('.menu')
+const menu = document.querySelector(".menu");
+const hamburguesa = document.querySelector(".menu .hamburguesa")
+const links = document.querySelector(".links")
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if ( window.pageYOffset > 199 ){
+      if(prevScrollpos > currentScrollPos) {
+          menu.style.top = "-100%";
+          menu.style.opacity = "0";
+          links.classList.remove("show")
+        } else {
+            menu.style.top = "25px";
+            menu.style.opacity = "1";
+        }
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+hamburguesa.addEventListener('click', ()=> {
+    console.log('click');
+    links.classList.toggle("show")
+    hamburguesa.classList.toggle("whiteLetters")
+})
