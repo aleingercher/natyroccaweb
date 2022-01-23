c70db215303cd24650172d8239d44db27993802d
Ale Ingercher <aleingercher@gmail.com>
2022-01-23 11:51:27 -0300

    puesto fondo textil menu

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 const menu = document.querySelector(".menu");
-const hamburguesa = document.querySelector(".menu .hamburguesa")
-const links = document.querySelector(".links")
+const hamburguesa = document.querySelector(".menu .hamburguesa");
+const links = document.querySelector(".links");
 
 let prevScrollpos = window.pageYOffset;
 
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if ( window.pageYOffset > 199 ){
-      if(prevScrollpos > currentScrollPos) {
-          menu.style.top = "-100%";
-          menu.style.opacity = "0";
-          links.classList.remove("show")
-        } else {
-            menu.style.top = "25px";
-            menu.style.opacity = "1";
-        }
-    }
-    prevScrollpos = currentScrollPos;
+const showMenuButton = () => {
+  menu.style.top = "25px";
+  menu.style.opacity = "1";
+  hamburguesa.classList.remove("whiteLetters");
+};
+const hideMenuButton = () => {
+  menu.style.top = "-100%";
+  menu.style.opacity = "0";
+  links.classList.remove("show");
 };
 
-hamburguesa.addEventListener('click', ()=> {
-    console.log('click');
-    links.classList.toggle("show")
-    hamburguesa.classList.toggle("whiteLetters")
-})
+window.addEventListener("DOMContentLoaded", (event) => {
+  hideMenuButton();
+});
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (window.pageYOffset > 199) {
+    if (prevScrollpos > currentScrollPos) {
+      hideMenuButton();
+    } else {
+      showMenuButton();
+    }
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+hamburguesa.addEventListener("click", () => {
+  console.log("click");
+  links.classList.toggle("show");
+  hamburguesa.classList.toggle("whiteLetters");
+});
