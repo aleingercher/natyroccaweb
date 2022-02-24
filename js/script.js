b616125b327c3b8e038edba55feb0d240ae386ca
Ale Ingercher <aleingercher@gmail.com>
2022-02-23 23:12:04 -0300

    puesto el boton de go to top, cambiadas las fotos de los botones y del nav

--- js/script.js
+++ js/script.js
@@ -42,6 +42,7 @@ document.addEventListener("click", (e) => {
   //     hideMenuButton();
   //   }
 });
+
 /* para que el menu se muestre al bajar y se esconda al subir */
 let prevScrollpos = window.pageYOffset;
 
@@ -55,10 +56,31 @@ window.onscroll = function () {
     }
   }
   prevScrollpos = currentScrollPos;
+
+  scrollFunction();
 };
 
-/* desplegar menu al ahcer click en el boton */
+/* desplegar menu al hacer click en el boton */
 hamburguesa.addEventListener("click", () => {
   nav.classList.toggle("show");
   hamburguesa.classList.toggle("hideLetters");
 });
+
+/* BACK TO TOP BUTTON */
+//Get the button:
+const mybutton = document.querySelector("#backToTopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.opacity = "0";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
