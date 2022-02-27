14216f0ed96418461c5b6009c7dcb04c3c08ee69
Ale Ingercher <aleingercher@gmail.com>
2022-02-27 16:48:44 -0300

    carousel de tintes, maqueta de bio

--- js/script.js
+++ js/script.js
@@ -74,13 +74,15 @@ const mybutton = document.querySelector("#backToTopButton");
 const scrollFunction = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.opacity = "1";
+    mybutton.style.cursor = 'pointer';
   } else {
     mybutton.style.opacity = "0";
+    mybutton.style.cursor = 'default';
   }
 };
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+const topFunction = ()=> {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
