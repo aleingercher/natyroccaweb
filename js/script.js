63011fb6423b644d7110b59e4df73b6804e79fbe
Ale Ingercher <aleingercher@gmail.com>
2022-03-24 22:56:32 -0300

    script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-/* Menu vendria a ser todo el menu. Es el selector mas abarcativo */
-const menu = document.querySelector(".menu");
 const hamburguesa = document.querySelector(".hamburguesa");
 const hamburguesaButton = document.querySelector(".hamburguesa > p");
-const nav = document.querySelector("#nav");
 const links = document.querySelectorAll(".nav > ul > li");
+const menu = document.querySelector(".menu"); //Menu vendria a ser todo el menu. Es el selector mas abarcativo
+const nav = document.querySelector("#nav");
 
 window.addEventListener("DOMContentLoaded", (event) => {
   hideMenuButton();
@@ -74,15 +73,15 @@ const mybutton = document.querySelector("#backToTopButton");
 const scrollFunction = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.opacity = "1";
-    mybutton.style.cursor = 'pointer';
+    mybutton.style.cursor = "pointer";
   } else {
     mybutton.style.opacity = "0";
-    mybutton.style.cursor = 'default';
+    mybutton.style.cursor = "default";
   }
 };
 
 // When the user clicks on the button, scroll to the top of the document
-const topFunction = ()=> {
+const topFunction = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
