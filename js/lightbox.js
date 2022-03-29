0f074450088c0c1c0521c80977b75acfda5caf43
Ale Ingercher <aleingercher@gmail.com>
2022-03-29 10:46:53 -0300

    lightbox funcionando

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,48 +1,53 @@
 // todos los select
 
 const fotosBroches = document.querySelectorAll(".fotosBroches > img"),
-fotosMuniecos = document.querySelectorAll(".fotosMuniecos > img"),
-fotosIlustraciones = document.querySelectorAll(".fotosIlustraciones > img"),
+  fotosMuniecos = document.querySelectorAll(".fotosMuniecos > img"),
+  fotosIlustraciones = document.querySelectorAll(".fotosIlustraciones > img"),
   previewBox = document.querySelector(".preview-box"),
   previewImg = previewBox.querySelector("img"),
+  imgContainer = document.querySelector(".imgContainer:not(.imagen)"),
   closeIcon = previewBox.querySelector(".fa-times"),
   shadow = document.querySelector(".shadow");
 
+document.querySelectorAll(".bio-wide div:not([class*='left'").forEach((e) => console.log(e));
+console.log(imgContainer);
 window.onload = () => {
-  const carouselDeFotos = (section)=> {
+  const carouselDeFotos = (section) => {
     for (let i = 0; i < section.length; i++) {
       let newIndex = i;
       let clickImgIndex;
+      const prevBtn = document.querySelector(".prev");
+      const nextBtn = document.querySelector(".next");
       section[i].onclick = (e) => {
         clickImgIndex = newIndex;
-        imgWidth = section[i].width;
-        imgHeight = section[i].height;
-        console.log(e)
-  
+
+        if (i == 0) {
+          prevBtn.style.display = "none";
+        }
+
+        if (i == section.length - 1) {
+          nextBtn.style.display = "none";
+        }
+
         const preview = () => {
           let selectedImageUrl = section[newIndex].src;
           previewImg.src = selectedImageUrl;
         };
-  
-        const prevBtn = document.querySelector(".prev");
-        const nextBtn = document.querySelector(".next");
-  
-        prevBtn.onclick = () => {
+
+        const prevBtnLogic = () => {
           newIndex--;
-          console.log(newIndex);
           if (newIndex == 0) {
             prevBtn.style.display = "none";
           }
           if (newIndex < section.length - 1) {
             nextBtn.style.display = "block";
           }
-  
+
           preview();
         };
-  
-        nextBtn.onclick = () => {
+
+        const nextBtnLogic = () => {
           newIndex++;
-          console.log(newIndex, section.length - 1);
           if (newIndex > 0) {
             prevBtn.style.display = "block";
           }
@@ -51,36 +56,56 @@ window.onload = () => {
           }
           preview();
         };
+
+        prevBtn.onclick = () => {
+          prevBtnLogic();
+        };
+
+        nextBtn.onclick = () => {
+          nextBtnLogic();
+        };
         preview();
-  
+
+        document.addEventListener("keydown", (e) => {
+          if (e.keyCode == 37 && (newIndex > 0)) {
+            prevBtnLogic();
+          }
+          if (e.keyCode == 39 && (newIndex < section.length - 1)) {
+            nextBtnLogic();
+          }
+        });
+
         previewBox.classList.add("show");
-        shadow.style.display = 'block';
-        document.querySelector('body').style.overflow = "hidden"
-  
+        shadow.style.display = "block";
+        document.querySelector("body").style.overflow = "hidden";
+
         // funcion que maneja el cierre del modal
         const closePhoto = () => {
           newIndex = clickImgIndex;
           previewBox.classList.remove("show");
           prevBtn.style.display = "block";
           nextBtn.style.display = "block";
-          shadow.style.display = 'none';
-          document.querySelector('body').style.overflow = "auto"
+          shadow.style.display = "none";
+          document.querySelector("body").style.overflow = "auto";
         };
-  
-  
+
         closeIcon.onclick = () => {
           closePhoto();
         };
-  
-        shadow.onclick = ()=> {
+
+        imgContainer.onclick = () => {
+          console.log("click");
           closePhoto();
-        }
+        };
+
+        shadow.onclick = () => {
+          closePhoto();
+        };
       };
     }
-  }
+  };
 
-  carouselDeFotos(fotosBroches)
-  carouselDeFotos(fotosMuniecos)
-  carouselDeFotos(fotosIlustraciones)
-
+  carouselDeFotos(fotosBroches);
+  carouselDeFotos(fotosMuniecos);
+  carouselDeFotos(fotosIlustraciones);
 };
